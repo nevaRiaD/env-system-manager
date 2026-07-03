a2563e0ff4f8b8243a9d338ebe32e62690e1a98f
nevaRiaD <jaycee.alipio@gmail.com>
2026-07-02 23:40:50 -0700

    feat: added read/write functions using HAL_I2C
    feat: added set mode function with enum
    
    feat: added i2c address for devAddress
    
    feat: added length to bme280_defs.h

--- Drivers/BME280/Inc/bme280.h
+++ Drivers/BME280/Inc/bme280.h
@@ -13,64 +13,65 @@
  * and any constraints on its lifecycle.
  */
 typedef struct bme280_cfg {
-    /* ===== MEMORY MAP CONFIG ===== */
+  /* ===== MEMORY MAP CONFIG ===== */
 
-    /* Mode Config
-     *
-     * 0b00:        Sleep Mode  - No measurements performed and
-     *                            power consumption at minimum.
-     * 0b01 & 0b10: Forced Mode - Single measurement is performed,
-     *                            cached, and then goes to sleep.
-     * 0b11:        Normal Mode - Automatic perpetual cycling between
-     *                            an active measurement period and
-     *                            an inactive measurement period.
-     */
-    uint8_t mode;           /* Power Settings for mode */
+  /* Mode Config
+   *
+   * 0b00:        Sleep Mode  - No measurements performed and
+   *                            power consumption at minimum.
+   * 0b01 & 0b10: Forced Mode - Single measurement is performed,
+   *                            cached, and then goes to sleep.
+   * 0b11:        Normal Mode - Automatic perpetual cycling between
+   *                            an active measurement period and
+   *                            an inactive measurement period.
+   */
+  uint8_t mode; /* Power Settings for mode */
 
-    /* Oversampling Config
-     * 0b000:  Skipped (output set to 0x80000)
-     * 0b001:  oversampling x 1
-     * 0b010:  oversampling x 2
-     * 0b011:  oversampling x 4
-     * 0b100:  oversampling x 8
-     * 0b101+: oversampling x 16
-     * 
-     * Note: Increasing oversampling increases latency
-     */
-    uint8_t os_hum_cfg;         /* Controls oversampling for humidity*/
-    uint8_t os_pres_cfg;        /* Controls oversampling for pressure */   
-    uint8_t os_temp_cfg;        /* Controls oversampling for temperature */
+  /* Oversampling Config
+   * 0b000:  Skipped (output set to 0x80000)
+   * 0b001:  oversampling x 1
+   * 0b010:  oversampling x 2
+   * 0b011:  oversampling x 4
+   * 0b100:  oversampling x 8
+   * 0b101+: oversampling x 16
+   * 
+   * Note: Increasing oversampling increases latency
+   */
+  uint8_t os_hum_cfg;   /* Controls oversampling for humidity */
+  uint8_t os_pres_cfg;  /* Controls oversampling for pressure */   
+  uint8_t os_temp_cfg;  /* Controls oversampling for temperature */
 
-    /* t_sb Config
-     * 0b000: 0.5 ms
-     * 0b001: 62.5 ms
-     * 0b010: 125 ms
-     * 0b011: 250 ms
-     * 0b100: 500 ms
-     * 0b101: 1000 ms
-     * 0b110: 10 ms
-     * 0b111: 20 ms
-     */
-    uint8_t t_sb;               /* Controls inactive duration for t_standby*/
+  /* t_sb Config
+   * 0b000: 0.5 ms
+   * 0b001: 62.5 ms
+   * 0b010: 125 ms
+   * 0b011: 250 ms
+   * 0b100: 500 ms
+   * 0b101: 1000 ms
+   * 0b110: 10 ms
+   * 0b111: 20 ms
+   */
+  uint8_t t_sb;  /* Controls inactive duration for t_standby*/
 
-    /* Filter Config
-     *
-     * 0b000: Filter off
-     * 0b001: Filter Coefficient = 2
-     * 0b010: Filter Coefficient = 4
-     * 0b011: Filter Coefficient = 8
-     * 0b100: Filter Coefficient = 16
-     */
-    uint8_t filter;             /* Controls time constant of the IIR filter */
+  /* Filter Config
+   *
+   * 0b000: Filter off
+   * 0b001: Filter Coefficient = 2
+   * 0b010: Filter Coefficient = 4
+   * 0b011: Filter Coefficient = 8
+   * 0b100: Filter Coefficient = 16
+   */
+  uint8_t filter;   /* Controls time constant of the IIR filter */
 
-    bool spi_enabled;        /* Enables 3-wire SPI interface when set to ‘1’ */
-    
-    /* ===== USER CONFIG ===== */
+  bool spi_enabled; /* Enables 3-wire SPI interface when set to ‘1’ */
+  
+  /* ===== USER CONFIG ===== */
 
-    bool hum_enabled;        /* Enables humidity; by default temp/pressure
-                                   are enabled but not humidity */
-    
-    I2C_HandleTypeDef *hi2c;    /* Handle for STM32 HAL H2C */
+  bool hum_enabled;  /* Enables humidity; by default temp/pressure
+                        are enabled but not humidity */
+  
+  I2C_HandleTypeDef *hi2c;  /* Handle for STM32 HAL H2C */
+  uint16_t i2c_address;     /* Select based SDO_LOW or SDO_HIGH */
 } bme280_cfg;
 
 /**
@@ -79,13 +80,22 @@ typedef struct bme280_cfg {
  * Addresses are based on BME280 memory map (pg 27)
  */
 typedef struct bme280_data {    
-    int32_t pressure;       /* addr: 0xF7, bit-format: 20 bits */
-    int32_t temperature;    /* addr: 0xFA, bit-format: 20 bits */
-    int32_t humidity;       /* addr: 0xFD, bit-format: 16 bits */
+  int32_t pressure;       /* addr: 0xF7, bit-format: 20 bits */
+  int32_t temperature;    /* addr: 0xFA, bit-format: 20 bits */
+  int32_t humidity;       /* addr: 0xFD, bit-format: 16 bits */
 } bme280_data;
 
+typedef enum {
+  sleep_mode = 0,
+  forced_mode = 1,
+  normal_mode = 3,
+} mode_t;
+
 int bme280_init(bme280_cfg *cfg, I2C_HandleTypeDef *hi2c);
+int bme280_read_data(bme280_data *data, const bme280_cfg cfg);
 int bme280_read_cfg(bme280_cfg *cfg);
-int bme280_read_data(bme280_data *data, const bme280_cfg cfg)
+int bme280_write_cfg(bme280_cfg *cfg);
+
+int bme280_set_mode(cfg, mode);
 
 #endif // BME280_H
--- Drivers/BME280/Src/bme280.c
+++ Drivers/BME280/Src/bme280.c
@@ -2,67 +2,126 @@
 #include "stm32f4xx_hal_i2c.h"
 
 /* static function prototypes */
-static int bme280_i2c_read_reg(uint8_t reg, uint8_t *buf, uint16_t len, I2C_HandleTypeDef *hi2c);
-static int bme280_i2c_write_reg(uint8_t reg, uint8_t val);
+static int bme280_i2c_read_reg(uint16_t memAddress, uint8_t *buf,
+                               uint16_t len, bme280_cfg *cfg);
+static int bme280_i2c_write_reg(uint16_t memAddress, uint8_t *buf,
+                                uint16_t len, bme280_cfg *cfg);
 
 int bme280_init(bme280_cfg *cfg, I2C_HandleTypeDef *hi2c)
 {
-	cfg->hi2c = hi2c;
-	cfg->spi_enabled = false;
+  cfg->hi2c = hi2c;
+  cfg->spi_enabled = false;
+  cfg->i2c_address = BME280_I2C_ADDR_SDO_LOW;
 
-	/* Read cfg of BME280 using I2C */
-	if (!bme280_i2c_read_cfg(cfg)) {
-		// TODO: Insert Console Error print statement
-		return 1;
-	}
+  /* Read cfg of BME280 using I2C */
+  if (!bme280_i2c_read_cfg(cfg)) {
+    // TODO: Insert Console Error print statement
+    return 1;
+  }
 }
 
-int bme280_read_data(bme280_data *data, bme280_cfg *cfg)
+int bme280_read_data(bme280_data *data, const bme280_cfg *cfg)
 {
-	uint16_t data_len = cfg->hum_enabled ? BME280_DATA_LEN_HUM : BME280_DATA_LEN_TEMP;
-	uint8_t raw[data_len];
-	bme280_i2c_read_reg(BME280_DATA_ADDR_START, raw, data_len, cfg->hi2c);
+  uint8_t raw[BME280_DATA_LEN_HUM];
+  uint16_t data_len = cfg->hum_enabled ? BME280_DATA_LEN_HUM : BME280_DATA_LEN_TEMP;
+  
+  if (bme280_i2c_read_reg(BME280_DATA_ADDR_START, raw,
+                          data_len, cfg->hi2c)) {
+    return 1; // I2C read error
+  }
 
-	/* Read buffer and append values */
-	
+  /* raw[0]=press_msb raw[1]=press_lsb raw[2]=press_xlsb<7:4> */
+  data->pressure = ((uint32_t)raw[0] << BME280_PRESS_MSB_SHIFT)  |
+                   ((uint32_t)raw[1] << BME280_PRESS_LSB_SHIFT)  |
+                   (raw[2] >> BME280_PRESS_XLSB_Pos);
+
+  /* raw[3]=temp_msb raw[4]=temp_lsb raw[5]=temp_xlsb<7:4> */
+  data->temperature = ((uint32_t)raw[3] << BME280_TEMP_MSB_SHIFT)  |
+                      ((uint32_t)raw[4] << BME280_TEMP_LSB_SHIFT)  |
+                      (raw[5] >> BME280_TEMP_XLSB_Pos);
+
+  // Check if humidity is enabled
+  if (cfg->hum_enabled) {
+    /* raw[6]=hum_msb raw[7]=hum_lsb */
+    data->humidity = ((uint32_t)raw[6] << BME280_HUM_MSB_SHIFT) | raw[7];
+  }
+  else {
+    data->humidity = 0;
+  }
+
+  return 0;
 }
 
 int bme280_i2c_read_cfg(bme280_cfg *cfg)
 {
-	/* 0xF2 -> 0xF5 , 4 bytes */
-	uint8_t raw[BME280_CFG_LEN];
-	bme280_i2c_read_reg(BME280_CFG_ADDR_START, raw, BME280_CFG_LEN, cfg->hi2c);
+  /* 0xF2 -> 0xF5 , 4 bytes */
+  uint8_t raw[BME280_CFG_LEN];
+  if (bme280_i2c_read_reg(BME280_CFG_ADDR_START, raw,
+                          BME280_CFG_LEN, cfg)) {
+    return 1; // I2C read error
+  }
 
-	for (int i = 0; i < BME280_CFG_LEN; i++) {
-		switch(i)
-		{
-		/* ctrl_hum: 0xF2 */
-		case 0:
-			cfg->os_hum_cfg = 1;
-			continue;
-		/* status: 0xF3 */
-		case 1:
-			/* nothing related to cfg */
-			continue;
-		/* ctrl_meas: 0xF4 */
-		case 2:
-			cfg->mode = 1;
-			cfg->os_pres_cfg = 1;
-			cfg->os_temp_cfg = 1;
-			continue;
-		/* config: 0xF5 */
-		case 3:
-			cfg->filter = 1;
-			cfg->t_sb = 1;
-			continue;
-		}
-	}
+  /* raw[0] = ctrl_hum */
+  cfg->os_hum_cfg  = (raw[0] & BME280_CTRL_HUM_OSRS_H_Msk) >> 
+                      BME280_CTRL_HUM_OSRS_H_Pos;
+
+  /* raw[1] = status - nothing for cfg */
+
+  /* raw[2] = ctrl_meas */
+  cfg->mode        = (raw[2] & BME280_CTRL_MEAS_MODE_Msk) >> 
+                      BME280_CTRL_MEAS_MODE_Pos;
+  cfg->os_pres_cfg = (raw[2] & BME280_CTRL_MEAS_OSRS_P_Msk) >> 
+                      BME280_CTRL_MEAS_OSRS_P_Pos;
+  cfg->os_temp_cfg = (raw[2] & BME280_CTRL_MEAS_OSRS_T_Msk) >> 
+                      BME280_CTRL_MEAS_OSRS_T_Pos;
+
+  /* raw[3] = config */
+  cfg->filter      = (raw[3] & BME280_CONFIG_FILTER_Msk) >> 
+                      BME280_CONFIG_FILTER_Pos;
+  cfg->t_sb        = (raw[3] & BME280_CONFIG_T_SB_Msk) >>
+                      BME280_CONFIG_T_SB_Pos;
+
+  return 0;
 }
 
-static int bme280_i2c_read_reg(uint8_t reg, uint8_t *buf, uint16_t len, I2C_HandleTypeDef *hi2c)
+int bme280_write_cfg(bme280_cfg *cfg)
 {
-	return HAL_I2C_Mem_Read(hi2c, uint16_t DevAddress,
-							uint16_t MemAddress, uint16_t MemAddSize, 
-							uint8_t *pData, uint16_t Size, uint32_t Timeout);
+  // ctrl_meas ctrl_hum
+  uint8_t ctrl_hum = cfg->os_hum_cfg & BME280_CTRL_HUM_OSRS_H_Msk;
+  uint8_t ctrl_meas = ((cfg->os_temp_cfg << BME280_CTRL_MEAS_OSRS_T_Pos) & BME280_CTRL_MEAS_OSRS_T_Msk) |
+                      ((cfg->os_pres_cfg << BME280_CTRL_MEAS_OSRS_P_Pos) & BME280_CTRL_MEAS_OSRS_P_Msk) |
+                      (cfg->mode & BME280_CTRL_MEAS_MODE_Msk);
+  uint8_t config = ((cfg->t_sb << BME280_CONFIG_T_SB_Pos) & BME280_CONFIG_T_SB_Msk) |
+                   ((cfg->filter << BME280_CONFIG_FILTER_Pos) & BME280_CONFIG_FILTER_Msk) |
+                   ((uint8_t)cfg->spi_enabled & BME280_CONFIG_SPI3W_EN_Msk);
+
+  /* config reg reliably accepts writes in sleep mode */
+  if (bme280_set_mode(cfg, sleep_mode)) {
+    return 1; // failed to set to sleep mode
+  }
+  if (bme280_i2c_write_reg(BME280_CONFIG_ADDR, &config, 1, cfg)) return 1;
+
+  /* ctrl_hum must be before ctrl_meas for writing */
+  if (bme280_i2c_write_reg(BME280_CTRL_HUM_ADDR, &ctrl_hum, 1, cfg)) return 1;
+  if (bme280_i2c_write_reg(BME280_CTRL_MEAS_ADDR, &ctrl_meas, 1, cfg)) return 1;
+
+  return 0;
 }
 
+static int bme280_i2c_read_reg(uint16_t memAddress, uint8_t *buf,
+                               uint16_t len, bme280_cfg *cfg)
+{
+  return HAL_I2C_Mem_Read(cfg->hi2c, cfg->i2c_address,
+                          memAddress, I2C_MEMADD_SIZE_8BIT, 
+                          buf, len, HAL_MAX_DELAY);
+}
+
+static int bme280_i2c_write_reg(uint16_t memAddress, uint8_t *buf,
+                                uint16_t len, bme280_cfg *cfg)
+{
+  return HAL_I2C_Mem_Write(cfg->hi2c, cfg->i2c_address,
+                           memAddress, I2C_MEMADD_SIZE_8BIT,
+                           buf, len, HAL_MAX_DELAY);
+}
+
+// TODO: ADD HAL_ERROR_MESSAGES
